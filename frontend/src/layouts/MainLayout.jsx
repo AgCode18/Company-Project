6065ba59a00fd579e4fd212d9e88582b0735e32d
Anuj Gour <anujgaur644@gmail.com>
2026-07-30 16:46:40 +0530

    Add initial admin layout and components for admin pages

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,28 +1,3 @@
-// import { Outlet } from "react-router-dom";
-// import { useState } from "react";
-// import Header from "../components/Header/Header";
-// import Footer from "../components/Footer/Footer";
-
-// const MainLayout = () => {
-
-
-//     return (
-//         <div className="flex">
-//             {/* Sidebar */}
-//             sidebar
-//             {/* Content */}
-//             <div className="flex flex-col h-screen w-full md:ml-64">
-//                 <Header />
-//                 <div className="flex-1 p-4 md:p-6 overflow-auto bg-gray-50">
-//                     <Outlet />
-//                     <Footer />
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default MainLayout;
 import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
 import Lenis from "lenis";
@@ -51,9 +26,6 @@ const MainLayout = () => {
 
   return (
     <div className="flex">
-      {/* Sidebar */}
-      {/* Remove if not needed */}
-      {/* sidebar */}
 
       <div className="flex flex-col min-h-screen w-full">
         <Header />
